4aa812c5963f0fcb2bc3518662943d1e1bd8d638
Tobias <5702338+T0biii@users.noreply.github.com>
2025-01-16 22:02:23 +0000

    add test with loadbalancing

--- ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/config-mode/model/admin/vpn.lua
+++ ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/config-mode/model/admin/vpn.lua
@@ -9,14 +9,25 @@ local s = f:section(Section, nil, translate(
 local enabled = s:option(Flag, "enabled", translate("Enabled"))
 enabled.default = uci:get('wireguard', 'mesh_vpn') and uci:get_bool('gluon', 'mesh_vpn', 'enabled')
 
+local loadbalancing = s:option(Flag, "loadbalancing", translate("Loadbalancing"))
+loadbalancing.default = uci:get('wireguard', 'mesh_vpn') and uci:get('gluon', 'mesh_vpn', 'loadbalancing')
+
 function f:write()
 	local vpn_enabled = false
 	if enabled.data then
 		vpn_enabled = true
 	end
 
+    local vpn_loadbalancing
+    if loadbalancing.data then
+        vpn_loadbalancing = true
+    else
+        vpn_loadbalancing = false
+    end
+
 	uci:section('wireguard', 'mesh_vpn', 'mesh_vpn', {
 		enabled = vpn_enabled,
+        loadbalancing = vpn_loadbalancing,
 	})
 
 	uci:commit('wireguard')
